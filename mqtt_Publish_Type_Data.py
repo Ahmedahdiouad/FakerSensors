2f0bce9d8dda95c6e6eb7fab3b112f93cd0465a4
AhmedAhdiouad <Ahmedahdiouad5@gmail.com>
2024-05-07 14:29:13 +0200

    change files

--- mqtt_Publish_Type_Data.py
+++ mqtt_Publish_Type_Data.py
@@ -9,12 +9,12 @@ from datetime import datetime
 # MQTT Settings
 MQTT_Broker = "test.mosquitto.org"
 MQTT_Port = 1883
-MQTT_Topic_A = "Factory/Machine1/1/A"
-MQTT_Topic_B = "Factory/Machine1/2/B"
-MQTT_Topic_W = "Factory/Machine1/3/W"
-MQTT_Topic_X = "Factory/Machine1/4/X"
-MQTT_Topic_Y = "Factory/Machine1/5/Y"
-MQTT_Topic_Z = "Factory/Machine1/6/Z"
+MQTT_Topic_Temperature = "Factory/Machine1/1/Temperature"
+MQTT_Topic_Humidity = "Factory/Machine1/2/Humidity"
+MQTT_Topic_Flow = "Factory/Machine1/3/Flow"
+MQTT_Topic_Position = "Factory/Machine1/4/Position"
+MQTT_Topic_PaintLevel = "Factory/Machine1/5/PaintLevel"
+MQTT_Topic_SurfaceQuality = "Factory/Machine1/6/SurfaceQuality"
 
 # Subscribe to all Sensors at Base Topic
 mqttc = mqtt.Client()
@@ -35,84 +35,84 @@ toggle = 0
 def publish_Fake_Sensor_Values_to_MQTT():
     threading.Timer(1.0, publish_Fake_Sensor_Values_to_MQTT).start()
     global toggle
-    fake = Faker()
+    
 
     if toggle == 0:
-        B_Fake_Value = float("{0:.2f}".format(random.uniform(50, 90.01)))
+        Humidity_Fake_Value = float("{0:.2f}".format(random.uniform(50, 90.01)))
 
-        B_Data = {}
-        B_Data['Sensor_ID'] = "Type-1"
-        B_Data['Date'] = datetime.today().strftime("%d-%b-%Y %H:%M:%S:%f")
-        B_Data['B'] = B_Fake_Value
-        B_json_data = json.dumps(B_Data)
+        Humidity_Data = {}
+        Humidity_Data['Sensor_ID'] = "Type-1"
+        Humidity_Data['Date'] = datetime.today().strftime("%d-%b-%Y %H:%M:%S:%f")
+        Humidity_Data['Humidity'] = Humidity_Fake_Value
+        Humidity_json_data = json.dumps(Humidity_Data)
 
-        print("Publishing fake B Value: " + str(B_Fake_Value) + "...")
-        publish_To_Topic(MQTT_Topic_B, B_json_data)
+        print("Publishing fake Humidity Value: " + str(Humidity_Fake_Value) + "...")
+        publish_To_Topic(MQTT_Topic_Humidity, Humidity_json_data)
         toggle = 1
 
     elif toggle == 1:
-        A_Fake_Value = float("{0:.2f}".format(random.uniform(30, 60.01)))
+        Temperature_Fake_Value = float("{0:.2f}".format(random.uniform(30, 60.01)))
 
-        A_Data = {}
-        A_Data['Sensor_ID'] = "Type-2"
-        A_Data['Date'] = datetime.today().strftime("%d-%b-%Y %H:%M:%S:%f")
-        A_Data['A'] = A_Fake_Value
-        A_json_data = json.dumps(A_Data)
+        Temperature_Data = {}
+        Temperature_Data['Sensor_ID'] = "Type-2"
+        Temperature_Data['Date'] = datetime.today().strftime("%d-%b-%Y %H:%M:%S:%f")
+        Temperature_Data['Temperature'] = Temperature_Fake_Value
+        Temperature_json_data = json.dumps(Temperature_Data)
 
-        print("Publishing fake A Value: " + str(A_Fake_Value) + "...")
-        publish_To_Topic(MQTT_Topic_A, A_json_data)
+        print("Publishing fake Temperature Value: " + str(Temperature_Fake_Value) + "...")
+        publish_To_Topic(MQTT_Topic_Temperature, Temperature_json_data)
         toggle = 2
 
     elif toggle == 2:
-        W_Fake_Value = float("{0:.2f}".format(random.uniform(0, 50.01)))
+        Flow_Fake_Value = float("{0:.2f}".format(random.uniform(0, 50.01)))
 
-        W_Data = {}
-        W_Data['Sensor_ID'] = "Type-3"
-        W_Data['Date'] = datetime.today().strftime("%d-%b-%Y %H:%M:%S:%f")
-        W_Data['W'] = W_Fake_Value
-        W_json_data = json.dumps(W_Data)
+        Flow_Data = {}
+        Flow_Data['Sensor_ID'] = "Type-3"
+        Flow_Data['Date'] = datetime.today().strftime("%d-%b-%Y %H:%M:%S:%f")
+        Flow_Data['Flow'] = Flow_Fake_Value
+        Flow_json_data = json.dumps(Flow_Data)
 
-        print("Publishing fake W Value: " + str(W_Fake_Value) + "...")
-        publish_To_Topic(MQTT_Topic_W, W_json_data)
+        print("Publishing fake Flow Value: " + str(Flow_Fake_Value) + "...")
+        publish_To_Topic(MQTT_Topic_Flow, Flow_json_data)
         toggle = 3
 
     elif toggle == 3:
-        X_Fake_Value = int(random.randint(0,1))
+        Position_Fake_Value = int(random.randint(0,1))
 
-        X_Data = {}
-        X_Data['Sensor_ID'] = "Type-4"
-        X_Data['Date'] = datetime.today().strftime("%d-%b-%Y %H:%M:%S:%f")
-        X_Data['X'] = X_Fake_Value
-        x_json_data = json.dumps(X_Data)
+        Position_Data = {}
+        Position_Data['Sensor_ID'] = "Type-4"
+        Position_Data['Date'] = datetime.today().strftime("%d-%b-%Y %H:%M:%S:%f")
+        Position_Data['Position'] = Position_Fake_Value
+        Position_json_data = json.dumps(Position_Data)
 
-        print("Publishing fake X Value: " + str(X_Fake_Value) + "...")
-        publish_To_Topic(MQTT_Topic_X, x_json_data)
+        print("Publishing fake Position Value: " + str(Position_Fake_Value) + "...")
+        publish_To_Topic(MQTT_Topic_Position, Position_json_data)
         toggle = 4
 
     elif toggle == 4:
-        Y_Fake_Value = float("{0:.2f}".format(random.uniform(0, 1000.01)))
+        PaintLevel_Fake_Value = float("{0:.2f}".format(random.uniform(0, 1000.01)))
 
-        Y_Data = {}
-        Y_Data['Sensor_ID'] = "Type-5"
-        Y_Data['Date'] = datetime.today().strftime("%d-%b-%Y %H:%M:%S:%f")
-        Y_Data['Y'] = Y_Fake_Value
-        y_json_data = json.dumps(Y_Data)
+        PaintLevel_Data = {}
+        PaintLevel_Data['Sensor_ID'] = "Type-5"
+        PaintLevel_Data['Date'] = datetime.today().strftime("%d-%b-%Y %H:%M:%S:%f")
+        PaintLevel_Data['PaintLevel'] = PaintLevel_Fake_Value
+        PaintLevel_json_data = json.dumps(PaintLevel_Data)
 
-        print("Publishing fake Y Value: " + str(Y_Fake_Value) + "...")
-        publish_To_Topic(MQTT_Topic_Y, y_json_data)
+        print("Publishing fake PaintLevel Value: " + str(PaintLevel_Fake_Value) + "...")
+        publish_To_Topic(MQTT_Topic_PaintLevel, PaintLevel_json_data)
         toggle = 5
 
     elif toggle == 5:
-        Z_Fake_Value = int(random.randint(0,1))
+        SurfaceQuality_Fake_Value = int(random.randint(0,1))
 
-        Z_Data = {}
-        Z_Data['Sensor_ID'] = "Type-6"
-        Z_Data['Date'] = datetime.today().strftime("%d-%b-%Y %H:%M:%S:%f")
-        Z_Data['Z'] = Z_Fake_Value
-        z_json_data = json.dumps(Z_Data)
+        SurfaceQuality_Data = {}
+        SurfaceQuality_Data['Sensor_ID'] = "Type-6"
+        SurfaceQuality_Data['Date'] = datetime.today().strftime("%d-%b-%Y %H:%M:%S:%f")
+        SurfaceQuality_Data['SurfaceQuality'] = SurfaceQuality_Fake_Value
+        SurfaceQuality_json_data = json.dumps(SurfaceQuality_Data)
 
-        print("Publishing fake Z Value: " + str(Z_Fake_Value) + "...")
-        publish_To_Topic(MQTT_Topic_Z, z_json_data)
+        print("Publishing fake SurfaceQuality Value: " + str(SurfaceQuality_Fake_Value) + "...")
+        publish_To_Topic(MQTT_Topic_SurfaceQuality, SurfaceQuality_json_data)
         toggle = 0
 
 
--- store_Sensor_Data_to_DB.py
+++ store_Sensor_Data_to_DB.py
@@ -13,7 +13,6 @@ class DatabaseManager():
     def __init__(self):
         print("Initializing DatabaseManager...")
         self.conn = sqlite3.connect(DB_Name)
-        print("connect to db")
         self.conn.execute('pragma foreign_keys = on')
         self.conn.commit()
         self.cur = self.conn.cursor()
@@ -28,108 +27,106 @@ class DatabaseManager():
         self.conn.close()
 
 # Function to save Temperature to DB Table
-# Function to save Temperature to DB Table
-def A_Data_Handler(jsonData):
+def Temperature_Data_Handler(jsonData):
     # Parse Data 
     json_Dict = json.loads(jsonData)
     SensorID = json_Dict['Sensor_ID']
     Data_and_Time = json_Dict['Date']
-    A = json_Dict['A']
+    Temperature = json_Dict['Temperature']
     
     # Push into DB Table
     dbObj = DatabaseManager()
-    dbObj.add_del_update_db_record("INSERT INTO A_Data (SensorID, Date_n_Time, A) VALUES (?,?,?)", [SensorID, Data_and_Time, A])
+    dbObj.add_del_update_db_record("INSERT INTO Temperature_Data (SensorID, Date_n_Time, Temperature) VALUES (?,?,?)", [SensorID, Data_and_Time, Temperature])
     del dbObj
-    print("Inserted A Data into Database.")
+    print("Inserted Temperature Data into Database.")
     print("")
 
 # Function to save Humidity to DB Table
-def B_Data_Handler(jsonData):
+def Humidity_Data_Handler(jsonData):
     # Parse Data
     json_Dict = json.loads(jsonData)
     SensorID = json_Dict['Sensor_ID']
     Data_and_Time = json_Dict['Date']
-    B = json_Dict['B']
+    Humidity = json_Dict['Humidity']
     
     # Push into DB Table
     dbObj = DatabaseManager()
-    dbObj.add_del_update_db_record("INSERT INTO B_Data (SensorID, Date_n_Time, B) VALUES (?,?,?)", [SensorID, Data_and_Time, B])
+    dbObj.add_del_update_db_record("INSERT INTO Humidity_Data (SensorID, Date_n_Time, Humidity) VALUES (?,?,?)", [SensorID, Data_and_Time, Humidity])
     del dbObj
-    print("Inserted B Data into Database.")
+    print("Inserted Humidity Data into Database.")
     print("")
 
 # Function to save Flow Rate to DB Table
-def W_Data_Handler(jsonData):
+def Flow_Data_Handler(jsonData):
     # Parse Data 
     json_Dict = json.loads(jsonData)
     SensorID = json_Dict['Sensor_ID']
     Data_and_Time = json_Dict['Date']
-    W = json_Dict['W']
+    Flow = json_Dict['Flow']
     
     # Push into DB Table
     dbObj = DatabaseManager()
-    dbObj.add_del_update_db_record("INSERT INTO W_Data (SensorID, Date_n_Time, W) VALUES (?,?,?)", [SensorID, Data_and_Time, W])
+    dbObj.add_del_update_db_record("INSERT INTO Flow_Data (SensorID, Date_n_Time, Flow) VALUES (?,?,?)", [SensorID, Data_and_Time, Flow])
     del dbObj
-    print("Inserted W Data into Database.")
+    print("Inserted Flow Data into Database.")
     print("")
 
-# Function to save X to DB Table
-def X_Data_Handler(jsonData):
+# Function to save Position to DB Table
+def Position_Data_Handler(jsonData):
     # Parse Data 
     json_Dict = json.loads(jsonData)
     SensorID = json_Dict['Sensor_ID']
     Data_and_Time = json_Dict['Date']
-    X = json_Dict['X']
+    Position = json_Dict['Position']
     
     # Push into DB Table
     dbObj = DatabaseManager()
-    dbObj.add_del_update_db_record("INSERT INTO X_Data (SensorID, Date_n_Time, X) VALUES (?,?,?)", [SensorID, Data_and_Time, X])
+    dbObj.add_del_update_db_record("INSERT INTO Position_Data (SensorID, Date_n_Time, Position) VALUES (?,?,?)", [SensorID, Data_and_Time, Position])
     del dbObj
-    print("Inserted X Data into Database.")
+    print("Inserted Position Data into Database.")
     print("")
 
-# Function to save Y to DB Table
-def Y_Data_Handler(jsonData):
+# Function to save PaintLevel to DB Table
+def PaintLevel_Data_Handler(jsonData):
     # Parse Data
     json_Dict = json.loads(jsonData)
     SensorID = json_Dict['Sensor_ID']
     Data_and_Time = json_Dict['Date']
-    Y = json_Dict['Y']
+    PaintLevel = json_Dict['PaintLevel']
     
     # Push into DB Table
     dbObj = DatabaseManager()
-    dbObj.add_del_update_db_record("INSERT INTO Y_Data (SensorID, Date_n_Time, Y) VALUES (?,?,?)", [SensorID, Data_and_Time, Y])
+    dbObj.add_del_update_db_record("INSERT INTO PaintLevel_Data (SensorID, Date_n_Time, PaintLevel) VALUES (?,?,?)", [SensorID, Data_and_Time, PaintLevel])
     del dbObj
-    print("Inserted Y Data into Database.")
+    print("Inserted PaintLevel Data into Database.")
     print("")
 
-# Function to save Z to DB Table
-def Z_Data_Handler(jsonData):
+# Function to save SurfaceQuality to DB Table
+def SurfaceQuality_Data_Handler(jsonData):
     # Parse Data
     json_Dict = json.loads(jsonData)
     SensorID = json_Dict['Sensor_ID']
     Data_and_Time = json_Dict['Date']
-    Z = json_Dict['Z']
+    SurfaceQuality = json_Dict['SurfaceQuality']
     
     # Push into DB Table
     dbObj = DatabaseManager()
-    dbObj.add_del_update_db_record("INSERT INTO Z_Data (SensorID, Date_n_Time, Z) VALUES (?,?,?)", [SensorID, Data_and_Time, Z])
+    dbObj.add_del_update_db_record("INSERT INTO SurfaceQuality_Data (SensorID, Date_n_Time, SurfaceQuality) VALUES (?,?,?)", [SensorID, Data_and_Time, SurfaceQuality])
     del dbObj
-    print("Inserted Z Data into Database.")
+    print("Inserted SurfaceQuality Data into Database.")
     print("")
 
 # Master Function to Select DB Function based on MQTT Topic
 def sensor_Data_Handler(Topic, jsonData):
-    print("im being called")
-    if Topic == "Factory/Machine1/1/A":
-        A_Data_Handler(jsonData)
-    elif Topic == "Factory/Machine1/2/B":
-        B_Data_Handler(jsonData)
-    elif Topic == "Factory/Machine1/3/W":
-        W_Data_Handler(jsonData)
-    elif Topic == "Factory/Machine1/4/X":
-        X_Data_Handler(jsonData)
-    elif Topic == "Factory/Machine1/5/Y":
-        Y_Data_Handler(jsonData)
-    elif Topic == "Factory/Machine1/6/Z":
-        Z_Data_Handler(jsonData)
+    if Topic == "Factory/Machine1/1/Temperature":
+        Temperature_Data_Handler(jsonData)
+    elif Topic == "Factory/Machine1/2/Humidity":
+        Humidity_Data_Handler(jsonData)
+    elif Topic == "Factory/Machine1/3/Flow":
+        Flow_Data_Handler(jsonData)
+    elif Topic == "Factory/Machine1/4/Position":
+        Position_Data_Handler(jsonData)
+    elif Topic == "Factory/Machine1/5/PaintLevel":
+        PaintLevel_Data_Handler(jsonData)
+    elif Topic == "Factory/Machine1/6/SurfaceQuality":
+        SurfaceQuality_Data_Handler(jsonData)
